4b845e07352b7352075b44401b39e9786cf0f69a
Daniel Bidulock <daniel@capitolhill.ca>
2017-11-20 17:19:40 -0500

    Get decent second draught

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ $(function () {
         var secondFeature = $('#features').offset().top;
         var scroll = $(window).scrollTop();
         if (scroll >= 150) {
-            $('.sticky-navigation').css({"background-color": '#ffd700'});
+            $('.sticky-navigation').css({"background-color": '#a67c00'});
         } else {
             $('.sticky-navigation').css({"background-color": 'transparent'});
         }
